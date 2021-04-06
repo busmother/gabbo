1fdfcf0efc44c864f3c87f51bd30f93cce58b5fb
Ian Marshall <imarshall14@gmail.com>
2021-04-06 16:24:24 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       devise_for :users
-      resources :chats, only [:index, :create]
-      resources :messages, only [:index, :create]
+      resources :chats, only: [:index, :create]
+      resources :messages, only: [:index, :create]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
