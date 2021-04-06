6914f45992752a455f7e2cb19838e4a4c3a29664
Ian Marshall <imarshall14@gmail.com>
2021-04-06 17:28:28 -0400

    update users controller and routes for index and show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,16 @@
-class Api::Vi::UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
 
     def index
         users = User.all
         render json: users
     end
 
-    def create
-        user = User.new(user_params)
-        if user.save
-            render json: user, status: :accepted
-        else
-            render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
-        end
-    end
+    # def create #devise does this offscreen
+    #     user = User.new(user_params)
+    #     if user.save
+    #         render json: user, status: :accepted
+    #     else
+    #         render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+    #     end
+    # end
 end
