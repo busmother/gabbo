dfae12c4365c8904edca54a26b1de5053ea0529d
Ian Marshall <imarshall14@gmail.com>
2021-04-06 18:06:25 -0400

    create user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,15 +2,8 @@ class Api::V1::UsersController < ApplicationController
 
     def index
         users = User.all
+        # render json: UserSerializer.new(@users)
         render json: users
+        
     end
-
-    # def create #devise does this offscreen
-    #     user = User.new(user_params)
-    #     if user.save
-    #         render json: user, status: :accepted
-    #     else
-    #         render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
-    #     end
-    # end
 end
