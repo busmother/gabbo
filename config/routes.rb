dc85ec58f80f1efce9276009ad9ed38725db7eae
Ian Marshall <imarshall14@gmail.com>
2021-04-06 16:22:57 -0400

    add namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :api do
+    namespace :v1 do
+      devise_for :users
+      resources :chats, only [:index, :create]
+      resources :messages, only [:index, :create]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
