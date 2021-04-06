076a194c4127cd9d331d441b4dd35f374259218d
Ian Marshall <imarshall14@gmail.com>
2021-04-06 12:23:22 -0400

    add body attribute to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ApplicationRecord
+    belongs_to :chats
+    belongs_to :user
+
+    validates_presence_of :body, :chat_id, :user_id
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_154234) do
+ActiveRecord::Schema.define(version: 2021_04_06_162130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_04_06_154234) do
     t.bigint "chat_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
     t.index ["chat_id"], name: "index_messages_on_chat_id"
   end
 
