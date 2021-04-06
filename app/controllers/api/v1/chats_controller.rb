95723714efd018e7f28ead8a4d2c1b39455365cf
Ian Marshall <imarshall14@gmail.com>
2021-04-06 16:48:26 -0400

    create basic chats controller

---
+++ app/controllers/api/v1/chats_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::ChatsController < ApplicationController
+
+    def index
+        chats = Chat.all
+        render json: chats
+    end
+
+    def create
+        chat = Chat.new(chat_params)
+        if chat.save
+            render json: chat, status: :accepted
+        else
+            render json: {errros: chat.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+end
