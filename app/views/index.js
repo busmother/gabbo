aae08c4ef9d1d1033b4271ea819b89428e3d0d19
Ian Marshall <imarshall14@gmail.com>
2021-04-07 12:32:56 -0400

    create basic chatMarkup

--- app/views/index.js
+++ app/views/index.js
@@ -1,10 +1,22 @@
 const endPoint = "http://localhost:3000/api/v1/chats"
+// const endPoint = "http://localhost:3000"
 
 document.addEventListener('DOMContentLoaded', () =>{
-    console.log("Hi Nilay");
+    getChats()
+})
+
+function getChats() {
     fetch(endPoint)
     .then(response => response.json())
     .then(chats => {
         console.log(chats);
+        chats.data.forEach(chat => {
+            const chatMarkup =`
+            <div class = "chat">
+                <h3>Chat between Users ${chat.attributes.recipient_id} and ${chat.attributes.sender_id}</h3>
+            </div>`;
+
+            document.querySelector('.grid-container').innerHTML += chatMarkup
+        })
     })
-})
+}
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       devise_for :users
+      # root 'index'
       resources :users, only: [:index, :show]
       resources :chats, only: [:index, :create] do
         resources :messages, only: [:index, :create]
