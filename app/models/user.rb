b3662f71d87aa66c901121626e391579b36d0063
Ian Marshall <imarshall14@gmail.com>
2021-04-07 17:59:02 -0400

    update user model with new associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :chats, :foreign_key => :sender_id
+  # has_many :received_messages, class_name: "Message", through: :chat foreign_key: "recipient_id"
+  # has_many :sent_messages, class_name: "Message", through: :chat foreign_key: "sender_id"
+  has_many :received_messages, class_name: "Message", through: :chats
+  has_many :sent_messages, class_name: "Message", through: :chats
 end
