513ea395c3b409ed00428e89d65d1ac0b417c77c
Ian Marshall <imarshall14@gmail.com>
2021-04-07 17:37:28 -0400

    update db to have three chats

--- app/serializers/chat_serializer.rb
+++ app/serializers/chat_serializer.rb
@@ -1,5 +1,9 @@
 class ChatSerializer
   include JSONAPI::Serializer
   attributes :messages, :sender_id, :recipient_id
-  has_many :messages
+  has_many :messages, belongs_to: :sender
+  has_many :messages, belongs_to: :recipient
+
+  # has_one :recipient
+  # has_one :sender
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,3 +2,6 @@ class UserSerializer
   include JSONAPI::Serializer
   attributes :name, :image, :index_chats_on_recipient_id, :index_chats_on_sender_id
 end
+
+#might need to make a serializer for recipient and another for sender specifically
+#google self referential relationships w/models // polymorphic relationships
