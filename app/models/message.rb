e54b4b06393e4bd378c34e10d09b235be71483ff
Ian Marshall <imarshall14@gmail.com>
2021-04-07 17:41:15 -0400

    update message model associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
-    belongs_to :chats
-    belongs_to :user
+    belongs_to :chat
+    belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
+    belongs_to :sender, class_name: "User", foreign_key: "sender_id"
 
     validates_presence_of :body, :chat_id, :user_id
 
