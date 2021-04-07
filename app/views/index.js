1bef540337124d8cff9fa2d3b03f654a6b838c0e
Ian Marshall <imarshall14@gmail.com>
2021-04-06 20:44:33 -0400

    add 'rack-cors' gem

--- app/views/index.js
+++ app/views/index.js
@@ -1,1 +1,10 @@
-console.log("in index.js")
+const endPoint = "http://localhost:3000/api/v1/chats"
+
+document.addEventListener('DOMContentLoaded', () =>{
+    console.log("Hi Nilay");
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(chats => {
+        console.log(chats);
+    })
+})
