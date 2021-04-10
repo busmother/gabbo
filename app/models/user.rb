8fa191f3457035c4e22f3d13d1a5bf3f46f582d0
Ian Marshall <imarshall14@gmail.com>
2021-04-10 12:43:04 -0400

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   # has_many :sent_messages, class_name: "Message", through: :chat foreign_key: "sender_id"
   has_many :received_messages, class_name: "Message", through: :chats
   has_many :sent_messages, class_name: "Message", through: :chats
+
+  validates :name, length: {minimum: 1}
+  validates_uniqueness_of :name
 end
